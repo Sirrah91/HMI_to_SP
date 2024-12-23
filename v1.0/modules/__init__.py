d08045762a7b2dbb0a1c87e79ab48b3fe2d0ceac
Sirrah <92817370+Sirrah91@users.noreply.github.com>
2024-12-23 15:22:43 +0100

    Update __init__.py

--- v1.0/modules/__init__.py
+++ v1.0/modules/__init__.py
@@ -8,12 +8,3 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 # Extend PATH to include LaTeX binaries
 os.environ["PATH"] += os.pathsep + "/usr/bin:/usr/share/texlive/bin"
-
-"""
-1) celkovy tok v oblasti okolo nove vznikele pory
-    - kolik je potreba na vytvoreni?
-    - v nejake cele oblasti okolo ni, ne jen uvnitr kontury
-2) podminky na casove trvani nove pory
-    - minimalne existuje min_time cca 3 snimky
-    - uvolnit oblast, pokud se tam nic nevyskytuje alespon max_time cca 10 snimku
-"""
