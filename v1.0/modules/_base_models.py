0b64c66387a0ba3305eaf1d2bf7cd5318836e7d8
Sirrah <92817370+Sirrah91@users.noreply.github.com>
2025-03-27 10:57:50 +0100

    Add files via upload

--- v1.0/modules/_base_models.py
+++ v1.0/modules/_base_models.py
@@ -14,8 +14,12 @@ def load_base_models(model_config_file: str = "/nfshome/david/NN/base_models.con
 
     if not model_names:
         model_names = ["/nfshome/david/NN/models/HMI_to_SOT/HMI-to-SOT_1000_20241128161222.weights.h5",
+                       "/nfshome/david/NN/models/HMI_to_SOT/HMI-to-SOT_1000_20250317091749.weights.h5",
                        "/nfshome/david/NN/models/HMI_to_SOT/HMI-to-SOT_0100_20241128161222.weights.h5",
+                       "/nfshome/david/NN/models/HMI_to_SOT/HMI-to-SOT_0100_20250317091749.weights.h5",
                        "/nfshome/david/NN/models/HMI_to_SOT/HMI-to-SOT_0010_20241128161222.weights.h5",
-                       "/nfshome/david/NN/models/HMI_to_SOT/HMI-to-SOT_0001_20241128161222.weights.h5"]
+                       "/nfshome/david/NN/models/HMI_to_SOT/HMI-to-SOT_0010_20250317091749.weights.h5",
+                       "/nfshome/david/NN/models/HMI_to_SOT/HMI-to-SOT_0001_20241128161222.weights.h5",
+                       "/nfshome/david/NN/models/HMI_to_SOT/HMI-to-SOT_0001_20250317091749.weights.h5"]
 
     return model_names
